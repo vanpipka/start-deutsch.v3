5fd6a21e9f61bc8f14859c150f4d6a4c9413ea59
vanpipka <vanpipka@gmail.com>
2026-02-21 22:20:34 +0300

    +

--- config/articles/utils.py
+++ config/articles/utils.py
@@ -3,6 +3,6 @@ def get_exam_rules_url(category, level):
     key = f"{category}_{level}"
     
     if key.lower() == "A1_schreiben".lower():
-        return "nemetskiy-a1-testy/schreiben/rules"
+        return "/nemetskiy-a1-testy/schreiben/rules"
     
     return None
