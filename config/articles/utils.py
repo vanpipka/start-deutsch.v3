c138978eb0a6e0026a1de065bc9cdecb995ece45
vanpipka <vanpipka@gmail.com>
2026-03-03 14:11:50 +0300

    some change

--- config/articles/utils.py
+++ config/articles/utils.py
@@ -3,4 +3,7 @@ def get_exam_rules_url(category, level):
     if not category or not level:
         return ""
     
+    if category == "all":
+        return f"/{level}/rules/"
+    
     return f"/{level}/rules/{category}/"
